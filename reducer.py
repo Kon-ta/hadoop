40b442fc6066d9d49cb3d67d85faab709b949b29
Yuta Konno <fre7isd@gmail.com>
2016-08-30 09:00:21 +0900

    reducer code

---
+++ reducer.py
@@ -0,0 +1,16 @@
+import sys
+
+(last_key, count) = (None, 0)
+
+for line in sys.stdin:
+        (key, val) = line.strip().split(":")
+
+        if last_key and last_key != key:
+                print "%s\t%s" % (last_key, count)
+                (last_key, count) = (key,int(val))
+        else:
+                last_key = key
+                count += int(val)
+
+if last_key:
+        print "%s\t%s" % (last_key, count)
