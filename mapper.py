0206d302277501f7d57ce16fcc25461db239d7bb
Yuta Konno <fre7isd@gmail.com>
2016-09-01 09:13:04 +0900

    mapper

--- mapper.py
+++ mapper.py
@@ -4,16 +4,35 @@ argvs = sys.argv
 argc = len(argvs)
 val=[]
 for line in sys.stdin:
-    num=0
+
     items = line.split("||")
-
+    Flag=True
     for x in items:
+        try:
+            x_name,x_ele = x.strip("\n").split("=")
+            if x_name == "SOURCE_PORT":
+                try:
+                    if int(x_ele)<1024:
+                        Flag=False
+                        break
+                except:
+                    pass
 
-        x_name,x_ele = x.strip("\n").split("=")
-        for y in range(1,argc):
-            if x_name == argvs[y]:
+            if x_name == "SOURCE_IP":
+                try:
+                    ip_1,ip_2,ip_3,ip_4 = x_ele.split(".")
+                    ip_4 = "0"
+                    x_ele = ".".join([ip_1,ip_2,ip_3,ip_4])
+                except:
+                    pass    
+
+            for y in argvs:
+                if x_name == y:
                     val.append(x_ele)
-    for x in val:
-        print(x),
-    print(": 1")
+        except:
+            pass
+    if len(val) !=0:
+        val.append("=1")
+        if Flag == True:
+            print "   ".join(val)
     val=[]
